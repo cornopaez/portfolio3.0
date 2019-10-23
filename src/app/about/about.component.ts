1a0afc2e93cfaa6d15fc10384df4df6f7ab5254d
Mauricio Paez <jose.m.paez@icloud.com>
2019-10-23 11:52:11 -0400

    Added about page and related content.

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component'
+import { Title }     from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { BACKGROUND, LANGUAGES, EXPERIENCE, EDUCATION } from './about'
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.sass']
+})
+export class AboutComponent implements OnInit {
+
+    currentContent = {
+    view_title: 'Mauricio_Páez | About',
+    navbar: {
+      title: 'About',
+      links: [
+        'Home',
+        'Projects',
+        'Contact'
+      ]
+    }
+  }
+
+  background = BACKGROUND
+  languages = LANGUAGES
+  experience = EXPERIENCE
+  education = EDUCATION
+
+  constructor(
+    private title : Title
+    ) { }
+
+  ngOnInit() {
+    // Set the title for this view
+    this.title.setTitle(this.currentContent.view_title)
+    // console.log(this.currentContent)
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'Home', 
     redirectTo: '', 
     pathMatch: 'full'
+  },
+  { 
+    path: 'About', 
+    component: AboutComponent 
   },
 ];
 
