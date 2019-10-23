8d7f7fdc08aba52f8e601e46b7458dcbf8e50dda
Mauricio Paez <mauriciopaez@MacBook-Pro.localdomain>
2019-10-23 11:16:14 -0400

    First commit of project: addded home page and styles.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent implements OnInit {
+
+  currentContent = {
+      view_title: 'Mauricio_Páez | Developer',
+      content: {
+        image: '../assets/close-up.png',
+        description: 'IT Business Analysis, Web Development' ,
+        description_links: [
+          'Projects',
+          'About',
+          'Contact'
+        ]
+      }
+    };
+
+  constructor(
+  	private title : Title
+  	) { }
+
+  ngOnInit() {
+  	// Set the title for this view
+    this.title.setTitle(this.currentContent.view_title)
+    // console.log(this.currentContent)
+  }
+
+}
