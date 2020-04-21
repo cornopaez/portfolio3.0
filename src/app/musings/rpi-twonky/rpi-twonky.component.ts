793b44c8739bbcacb55fe86fd180bfa57272963e
Mauricio Paez <jose.m.paez@icloud.com>
2020-04-20 20:23:04 -0400

    Added content for rpi-twonky musing and adjusted styles.

--- src/app/musings/rpi-twonky/rpi-twonky.component.ts
+++ src/app/musings/rpi-twonky/rpi-twonky.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rpi-twonky.component.sass']
 })
 export class RpiTwonkyComponent implements OnInit {
+  currentContent = {
+    content: {
+      icons: [
+        'fab fa-raspberry-pi',
+        'fas fa-terminal'
+      ],
+      twonky: '../../../../assets/twonkyicon-120x120.png',
+      image:'../../../../assets/damir-kopezhanov-P4me0QE8Wlk-unsplash.jpg'
+    }
+  }
 
   constructor() { }
 
