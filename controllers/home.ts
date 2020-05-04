5c3ca03475eb6831597b4af3414a3f795f1f7fbe
Mauricio Paez <jose.m.paez@icloud.com>
2020-05-04 09:10:02 -0400

    Fixed bug where page would be server insecurely.

--- controllers/home.ts
+++ controllers/home.ts
@@ -2,16 +2,19 @@ import { Request, Response } from "express";
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { join } from 'path';
+import * as dotenv from 'dotenv'
 
 export const home = (req: Request, res: Response) => {
+  dotenv.config()
   const distFolder = join(process.cwd(), 'dist/portfolio3/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Check for secure connection
   if (req.headers["x-forwarded-proto"] !== "https" && process.env.NODE_ENV == 'production' && !req.secure){
-      console.log("Forcing HTTPS...")
-      res.redirect('https://'+req.hostname+req.url);
+    console.log("Forcing HTTPS...")
+    res.redirect('https://'+req.hostname+req.url);
+  } else  {
+    // Send page if everything is OK
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   }
-
-  res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
 }
