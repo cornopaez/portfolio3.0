8c0ecc14f1294057a83e03ca8d9c23d71a6dec31
Mauricio Paez <jose.m.paez@icloud.com>
2020-11-08 13:46:10 -0500

    Improved test coverage for projects-details.component.

--- src/app/projects/projects-details/projects-details.component.spec.ts
+++ src/app/projects/projects-details/projects-details.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
-import { ProjectsDetailsComponent } from './projects-details.component';
+import { ProjectsDetailsComponent } from './projects-details.component'
 import { ProjectDetails } from './project-details'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router'
 import { of } from 'rxjs'
 
 describe('ProjectsDetailsComponent', () => {
-  let component: ProjectsDetailsComponent;
-  let fixture: ComponentFixture<ProjectsDetailsComponent>;
+  let component: ProjectsDetailsComponent
+  let fixture: ComponentFixture<ProjectsDetailsComponent>
+  let compiled
   const projectDetails = {
     project: {
-      "_id" : "5db2ecfa1ae6656aad947259",
       "card" : {
         "title" : "Portfolio 3.0",
         "subtitle" : "",
@@ -38,10 +38,11 @@ describe('ProjectsDetailsComponent', () => {
             "devicon-mongodb-plain-wordmark"
           ],
           "image" : "./assets/programming.jpg",
-          "description" : "<i>This a moderate upgrade to Portfolio 2.0. It's been adapted to whatever the current version of Angular is (as of October 2019). Not much has changed: the design, architecture, and most source code is the same. Original description follows. </i> </br> </br>This a serious upgrade to my first Portfolio. Built from the ground up using Angular-CLI and Angular 2 (or is it 4?), it showcases my skills to build a functional site using a fair amount of its new features. There's a bit of everything: child routes, guards, and resolvers. The site makes use of a MEAN stack and the content for a few views comes from a MongoDB. I'm proud of how it turned out. Of course, the design is also mine (which explains why it's so sparse).",
+          "description" : "<i>This a moderate upgrade to Portfolio 2.0. It's been adapted to whatever the current version of Angular is (as of October 2019). Not much has changed: the design, architecture, and most source code is the same. Original description follows. </i> <br> <br>This a serious upgrade to my first Portfolio. Built from the ground up using Angular-CLI and Angular 2 (or is it 4?), it showcases my skills to build a functional site using a fair amount of its new features. There's a bit of everything: child routes, guards, and resolvers. The site makes use of a MEAN stack and the content for a few views comes from a MongoDB. I'm proud of how it turned out. Of course, the design is also mine (which explains why it's so sparse).",
           "source" : "https://github.com/cornopaez/portfolio3.0",
-          "footer" : "Made by: Mauricio Páez - Pittsburgh 2018, 2019 - Photo by <a href=\"https://unsplash.com/photos/_yMciiStJyY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText\">Maik Jonietz on Unsplash</a>",
-          "imageStyle" : "portfolio3"
+          "footer" : "Made by: Mauricio Páez - Pittsburgh 2018, 2019 - Photo by <a href=\"https://unsplash.com/photos/_yMciiStJyY?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText\">Maik Jonietz on Unsplash</a>",
+          "imageStyle" : "portfolio3",
+          "link": "/"
         },
         "contetn" : {
           "image" : "./assets/programming.jpg"
@@ -66,13 +67,62 @@ describe('ProjectsDetailsComponent', () => {
       ],
       declarations: [ ProjectsDetailsComponent ]
     })
-    .compileComponents();
-    fixture = TestBed.createComponent(ProjectsDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    .compileComponents()
+    fixture = TestBed.createComponent(ProjectsDetailsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    compiled = fixture.debugElement.nativeElement
+  }))
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should have the back button',()=>{
+    expect(compiled.querySelector("a.btn.btn-link[href='/Projects']").textContent).toEqual(' Back to Projects')
+  })
+
+  describe('Content of view', ()=>{
+    const project = projectDetails.project.view
+    it('should have correct class in jumbotron',()=>{
+      expect(compiled.querySelector(".jumbotron." + project.content.imageStyle)).toBeTruthy()
+    })
+
+    it('should have icons in jumbotron footnotes',()=>{
+      expect(compiled.querySelector(".jumbotron .footnotes h4").textContent).toEqual('Tech used')
+      for (var i = 0; i < project.content.icons.length; ++i) {
+         expect(compiled.querySelector(".jumbotron .footnotes ul li i[class='" + project.content.icons[i] + "']")).toBeTruthy()
+      }
+    })
+
+    it('should have correct title in .project-detail',()=>{
+      expect(compiled.querySelector("div.project-detail h1").textContent).toEqual(project.title)
+    })
+
+    it('should render the correct project description HTML', ()=>{
+      /*
+        TODO: Update the data in the db for content.description as some </br> are missing the backslash that results when rendered.
+      */
+      expect(compiled.querySelector('div.project-detail div.project-container div.project-points p.project-description').innerHTML).toEqual(project.content.description)
+    })
+
+    it('should have a link to the source code of the project',()=>{
+      expect(compiled.querySelector('div.project-detail div.project-container div.project-points p a[href="' + project.content.source + '"]')).toBeTruthy()
+      expect(compiled.querySelector('div.project-detail div.project-container div.project-points p a[href="' + project.content.source + '"]').textContent).toEqual(project.content.source)
+    })
+
+    it('should have the correct project footer HTML',()=>{
+      /*
+        TODO: Update the footer text to HTML-encode all appropriate chars
+      */
+      expect(compiled.querySelector('div.project-detail div.project-container div.project-points p.project-footer').innerHTML).toEqual(project.content.footer)
+    })
+
+    it('should have a link to the compiled project', ()=>{
+      /*
+        TODO: Update model of projects in db to include links for all.
+      */
+      expect(compiled.querySelector('div.project-detail div.project-container div.project-points a.btn.btn-primary[href="' + project.content.link + '"]')).toBeTruthy()
+    })
+  })
+})
