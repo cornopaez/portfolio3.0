461f0a3bca7b58c4d3469d90fcd2d1dca6ff1195
Mauricio Paez <jose.m.paez@icloud.com>
2020-11-11 13:39:39 -0500

    Improved test coverage on contact-home.component.

--- src/app/contact/contact-home/contact-home.component.spec.ts
+++ src/app/contact/contact-home/contact-home.component.spec.ts
@@ -1,25 +1,79 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ContactHomeComponent } from './contact-home.component';
+import { ContactHomeComponent } from './contact-home.component'
 
 describe('ContactHomeComponent', () => {
-  let component: ContactHomeComponent;
-  let fixture: ComponentFixture<ContactHomeComponent>;
+  let component: ContactHomeComponent
+  let fixture: ComponentFixture<ContactHomeComponent>
+  let compiled
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ContactHomeComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactHomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ContactHomeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    compiled = fixture.debugElement.nativeElement
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  describe('Content of view',()=>{
+    describe('Social links',()=>{
+      it('should have a link for twitter',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.social1 a')
+        expect(link.textContent).toEqual('Go to Twitter')
+        expect(link.getAttribute('href')).toEqual('https://www.twitter.com/cornopaez')
+      })
+
+      it('should NOT have a link for fb',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.social2 a.disabled')
+        expect(link.textContent).toEqual('Go to Facebook')
+        expect(link.getAttribute('href')).toBeNull()
+      })
+
+      it('should have a link for angellist',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.social3 a')
+        expect(link.textContent).toEqual('Go to AngelList')
+        expect(link.getAttribute('href')).toEqual('https://angel.co/jose-m-paez')
+      })
+
+      it('should have an email link',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.social4 a')
+        expect(link.textContent).toEqual('Send Email')
+        expect(link.getAttribute('href')).toEqual('mailto:contact@cornopaez.com')
+      })
+
+      it('should have a link to LinkedIn',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.social5 a')
+        expect(link.textContent).toEqual('Go to LinkedIn')
+        expect(link.getAttribute('href')).toEqual('https://www.linkedin.com/in/cornopaez')
+      })
+
+      it('should have a picture of the pup',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.corgi')
+        expect(link).toBeTruthy()
+        expect(link.querySelector('img.card-img')).toBeTruthy()
+      })
+
+      it('should have a link for the comments form',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.comments a')
+        expect(link.textContent).toEqual('Show me the form')
+        expect(link.getAttribute('routerLink')).toEqual('/Contact/form')
+      })
+
+      it('should have a leftover card',()=>{
+        const link = compiled.querySelector('div.card-columns div.card.leftover')
+        expect(link).toBeTruthy()
+        expect(link.querySelector('small[class="text-muted"]')).toBeTruthy()
+      })
+    })
+  })
+})
