e93cdff51cb469d522ab44fdf1b59d7532072fb2
Mauricio Paez <jose.m.paez@icloud.com>
2020-11-11 13:14:29 -0500

    Increased test coverage in contact-success.component.

--- src/app/contact/contact-success/contact-success.component.spec.ts
+++ src/app/contact/contact-success/contact-success.component.spec.ts
@@ -1,25 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ContactSuccessComponent } from './contact-success.component';
+import { ContactSuccessComponent } from './contact-success.component'
 
 describe('ContactSuccessComponent', () => {
-  let component: ContactSuccessComponent;
-  let fixture: ComponentFixture<ContactSuccessComponent>;
+  let component: ContactSuccessComponent
+  let fixture: ComponentFixture<ContactSuccessComponent>
+  let compiled
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ContactSuccessComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactSuccessComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ContactSuccessComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    compiled = fixture.debugElement.nativeElement
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  describe('Content of view', ()=>{
+    it('should have the correct icon',()=>{
+      const icon = compiled.querySelector('div.contact_success div.success_text i[class="fa-thumbs-up fas"]')
+      expect(icon).toBeTruthy()
+    })
+
+    it('should have a link to the game', ()=>{
+      const link = compiled.querySelector('div.contact_success div.success_text p a[routerLink="/Projects/TheMurdersintheRueMorge"]')
+      expect(link).toBeTruthy()
+    })
+
+    it('should have a link to baseUrl',()=>{
+      const link = compiled.querySelector('div.contact_success div.success_text p a[routerLink="/"]')
+      expect(link).toBeTruthy()
+    })
+  })
+})
