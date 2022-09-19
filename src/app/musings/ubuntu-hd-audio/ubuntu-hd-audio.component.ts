73c37e63d51c7deacac3b4d4ae07d3d64757f416
Mauricio Paez <jose.m.paez@icloud.com>
2022-09-19 15:24:30 -0400

    Partial ubuntu hd audio guide has been created.

--- src/app/musings/ubuntu-hd-audio/ubuntu-hd-audio.component.ts
+++ src/app/musings/ubuntu-hd-audio/ubuntu-hd-audio.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ubuntu-hd-audio.component.sass']
 })
 export class UbuntuHdAudioComponent implements OnInit {
+  currentContent = {
+    content: {
+      icons: [
+        'fa-brands fa-ubuntu',
+        'fa-terminal fas'
+      ],
+      hd_audio: 'https://www.jas-audio.or.jp/jas_cms/wp-content/themes/jas2022/old/img/jas/jas-hires-logo/jas-hires-logo_clip_image004.png',
+      alsamixer: [
+        '../../../../assets/alsamixer-1.png',
+        '../../../../assets/alsamixer-2.png',
+        '../../../../assets/alsamixer-3.png',
+      ]
+    }
+  }
 
   constructor() { }
 
