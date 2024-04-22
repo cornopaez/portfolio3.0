2bc8822625d4dea41e10faf3ab4c122b258d76a9
Mauricio Paez <jose.m.paez@icloud.com>
2024-04-22 07:20:58 -0400

    Saving work so far. Updated new musing content and site map.

--- src/app/musings/tls-for-local-lab/tls-for-local-lab.component.ts
+++ src/app/musings/tls-for-local-lab/tls-for-local-lab.component.ts
@@ -13,6 +13,32 @@ export class TlsForLocalLabComponent {
         'fa-terminal fas'
       ],
       twonky: '../../../../assets/twonkyicon-120x120.png'
-    }
+    },
+    acme_commands: [
+      {
+        parameter: "--dns",
+        description: "This configures the process used to validate ownership of your domain. Specifically, the `dns_cf` value uses a Cloudflare-specific process."
+      },
+      {
+        parameter: "-d",
+        description: "We'll need at least two of these. One for your domain, `mycustomdomain.local` in our case, and one for type of certificate we want, `'*.mycustomdomain.local'` in our case. You can certainly specify a single subdomain here instead of a wildcard one if that suits your needs. You can also add as many other subdomains as you want. To that end, the certificate created can be used for any of the subdomains you add."
+      },
+      {
+        parameter: "--server",
+        description: "The Certificate Authority (CA) to be used. `acme.sh` defaults to `ZeroSSL.com CA` but I like `Let's Encrypt` for this process."
+      },
+      {
+        parameter: "--pre-hook",
+        description: "The custom `bash` script to run before the certificates are installed or renewed. Useful if services need to be stopped or files need to be backed up to prevent issues."
+      },
+      {
+        parameter: "--post-hook",
+        description: "The customer `bash` script to run after the certificates are installed or renewd. Useful to bring back services online or moving files in place. "
+      }
+    ],
+    columnsToDisplay: [
+      'parameter', 
+      'description'
+      ]
   }
 }
